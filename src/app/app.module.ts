232de34effb3ed38927f392a4f388607938987e9
DaniAvolio <avoliodaniele9@gmail.com>
2023-08-01 21:58:08 +0200

    fix : fix sewtup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomepageComponent } from './components/homepage/homepage.component';
-
 @NgModule({
   declarations: [
     AppComponent
